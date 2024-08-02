3ee25bf21fc7e2fb97f3de051395750f8f153c96
SomjaniNileshMulchand <somjaninilesh@gmail.com>
2024-08-02 20:27:23 +0530

    HTML CSS JAVASCRIPT

--- javascript.js
+++ javascript.js
@@ -1,41 +1,41 @@
-// const dark = document.querySelector(".dark");
-// const night = document.querySelector(".night");
-// const light = document.querySelector(".light");
-// const sidebar = document.querySelector(".sidebar");
-// const activeListItem = document.querySelector(".list-item.active");
-
-// dark.addEventListener("click", ()=>{
-//     sidebar.className = "sidebar";
-//     activeListItem = "list-item active"
-// })
-
-// night.addEventListener("click", ()=>{
-//     sidebar.className = "sidebar night";
-//     activeListItem = "list-item active night"
-// })
-
-// light.addEventListener("click", ()=>{
-//     sidebar.className = "sidebar light";
-//     activeListItem = "list-item active light"
-// })
-
 const dark = document.querySelector(".dark");
 const night = document.querySelector(".night");
 const light = document.querySelector(".light");
 const sidebar = document.querySelector(".sidebar");
 const activeListItem = document.querySelector(".list-item.active");
 
-dark.addEventListener("click", () => {
-  sidebar.className = "sidebar";
-  activeListItem.className = "list-item";
-});
+dark.addEventListener("click", ()=>{
+    sidebar.className = "sidebar";
+    activeListItem = "list-item active"
+})
 
-night.addEventListener("click", () => {
+night.addEventListener("click", ()=>{
     sidebar.className = "sidebar night";
-    activeListItem.className = "list-item night";
-});
+    activeListItem = "list-item active night"
+})
 
-light.addEventListener("click", () => {
+light.addEventListener("click", ()=>{
     sidebar.className = "sidebar light";
-    activeListItem.className = "list-item light";
-});
+    activeListItem = "list-item active light"
+})
+
+// const dark = document.querySelector(".dark");
+// const night = document.querySelector(".night");
+// const light = document.querySelector(".light");
+// const sidebar = document.querySelector(".sidebar");
+// const activeListItem = document.querySelector(".list-item.active");
+
+// dark.addEventListener("click", () => {
+//   sidebar.className = "sidebar";
+//   activeListItem.className = "list-item";
+// });
+
+// night.addEventListener("click", () => {
+//     sidebar.className = "sidebar night";
+//     activeListItem.className = "list-item night";
+// });
+
+// light.addEventListener("click", () => {
+//     sidebar.className = "sidebar light";
+//     activeListItem.className = "list-item light";
+// });
