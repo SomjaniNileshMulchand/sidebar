ea34249e92c82922c46c33055636d59abfab8780
SomjaniNileshMulchand <somjaninilesh@gmail.com>
2024-08-08 19:38:34 +0530

    HTML CSS JAVASCRIPT

--- javascript.js
+++ javascript.js
@@ -2,40 +2,23 @@ const dark = document.querySelector(".dark");
 const night = document.querySelector(".night");
 const light = document.querySelector(".light");
 const sidebar = document.querySelector(".sidebar");
-const activeListItem = document.querySelector(".list-item.active");
+ 
 
 dark.addEventListener("click", ()=>{
-    sidebar.className = "sidebar";
-    activeListItem = "list-item active"
+    sidebar.style.backgroundColor= "#111827";
+    sidebar.style.color= "#f3f4f6"; 
+     
 })
 
 night.addEventListener("click", ()=>{
-    sidebar.className = "sidebar night";
-    activeListItem = "list-item active night"
+    sidebar.style.backgroundColor= "#312e81";
+    sidebar.style.color= "#f3f4f6"; 
+     
 })
 
 light.addEventListener("click", ()=>{
-    sidebar.className = "sidebar light";
-    activeListItem = "list-item active light"
+    sidebar.style.backgroundColor= "#f3f4f6";
+    sidebar.style.color= "#111827";  
 })
 
-// const dark = document.querySelector(".dark");
-// const night = document.querySelector(".night");
-// const light = document.querySelector(".light");
-// const sidebar = document.querySelector(".sidebar");
-// const activeListItem = document.querySelector(".list-item.active");
-
-// dark.addEventListener("click", () => {
-//   sidebar.className = "sidebar";
-//   activeListItem.className = "list-item";
-// });
-
-// night.addEventListener("click", () => {
-//     sidebar.className = "sidebar night";
-//     activeListItem.className = "list-item night";
-// });
-
-// light.addEventListener("click", () => {
-//     sidebar.className = "sidebar light";
-//     activeListItem.className = "list-item light";
-// });
+ 
